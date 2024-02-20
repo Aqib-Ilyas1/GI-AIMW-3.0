1409cb313df17814aa0a4c235f1f4f1e8dfcc990
Aqib Ilyas <aqibilyas289@gmail.com>
2024-02-20 10:47:23 +0500

    Exercise no 7
    Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.

---
+++ Module (1) - TypeScript/Ex#7-(Number_Eight)/main.js
@@ -0,0 +1,12 @@
+console.log("By Addition (5+3) \n");
+var resultbyadd = 5 + 3;
+console.log(resultbyadd + "\n");
+console.log("By Substraction (16-8) \n");
+var resultbysub = 16 - 8;
+console.log(resultbysub + "\n");
+console.log("By Multiplication (4*2) \n");
+var resultbymul = 4 * 2;
+console.log(resultbymul + "\n");
+console.log("By Division (64/8) \n");
+var resultbydiv = 64 / 8;
+console.log(resultbydiv);
