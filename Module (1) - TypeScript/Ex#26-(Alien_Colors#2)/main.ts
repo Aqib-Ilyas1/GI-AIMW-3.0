710012061294d909d4274b5c228e920bacf412f4
Aqib Ilyas <aqibilyas289@gmail.com>
2024-02-20 11:48:26 +0500

    Exercise no 26

---
+++ Module (1) - TypeScript/Ex#26-(Alien_Colors#2)/main.ts
@@ -0,0 +1,9 @@
+var alien_color= ["green","yellow", "red"];
+var color_found = "green";
+if(color_found == alien_color[0]){
+    console.log("You just earned 5 points for shooting the alien. \n")
+} else if(color_found != alien_color[0]){
+    console.log("You just earned 10 points. \n")  // no output
+}else{
+    console.log("")     //No Output
+}
