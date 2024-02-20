66e228c40da693c52a481cfdeb249d17680b2725
Aqib Ilyas <aqibilyas289@gmail.com>
2024-02-20 10:54:46 +0500

    Exercise no 18
    Seeing the World: Think of at least five places in the world you’d like to visit.
    • Store the locations in a array. Make sure the array is not in alphabetical order.
    
    • Print your array in its original order.
    
    • Print your array in alphabetical order without modifying the actual list.
    
    • Show that your array is still in its original order by printing it.
    
    • Print your array in reverse alphabetical order without changing the order of the original list.
    
    • Show that your array is still in its original order by printing it again.
    
    • Reverse the order of your list. Print the array to show that its order has changed.
    
    • Reverse the order of your list again. Print the list to show it’s back to its original order.
    
    • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
    
    • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.

---
+++ Module (1) - TypeScript/Ex#18-(Seeing_the-World)/main.js
@@ -0,0 +1,4 @@
+var locations = ["Makkah", "Madina", "Kashmir", "Al-Aqsa", "Turkey"];
+console.log("Original sequence: " + locations + "\n");
+console.log("Sequence after alphabetic order: " + locations.sort() + "\n");
+console.log("Sequence after reverse-alphabetic order: " + locations.reverse());
