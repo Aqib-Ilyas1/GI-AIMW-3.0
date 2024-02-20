403913497f7734b6e71bc97f8492747ab0966728
Aqib Ilyas <aqibilyas289@gmail.com>
2024-02-20 10:58:04 +0500

    Exercise no 22
    Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.

---
+++ Module (1) - TypeScript/Ex#22-(Intentional_Error)/main.js
@@ -0,0 +1,4 @@
+var arr = ["Aqib", "Majid", "Nasir", "Mustafa", "Saqib", "Sohail", "Ahmed", "Noman", "Sajid", "Hamza"];
+// _____________________ Index error __________________
+// console.log(arr[10])
+console.log(arr[5]);
