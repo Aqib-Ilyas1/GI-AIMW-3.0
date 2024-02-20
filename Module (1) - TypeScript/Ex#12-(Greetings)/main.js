82fc260d1e6babe4ccb29752f111c5364978bd55
Aqib Ilyas <aqibilyas289@gmail.com>
2024-02-20 10:50:50 +0500

    Exercise no 12
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Module (1) - TypeScript/Ex#12-(Greetings)/main.js
@@ -0,0 +1,18 @@
+var names = ["Ali", "Asif", "Noman", "Shafique", "Wasif"];
+//                           ********************* Pattern 1*********************
+console.log("1. " + names[0] + "\n");
+console.log("Hi, " + names[0] + " I hope you will be fine! \n");
+console.log("2. " + names[1] + "\n");
+console.log("Hi, " + names[1] + " I hope you will be fine! \n");
+console.log("3. " + names[2] + "\n");
+console.log("Hi, " + names[2] + " I hope you will be fine! \n");
+console.log("4. " + names[3] + "\n");
+console.log("Hi, " + names[3] + " I hope you will be fine! \n");
+console.log("5. " + names[4] + "\n");
+console.log("Hi, " + names[4] + " I hope you will be fine! \n");
+//                           ********************* Pattern 2*********************
+console.log("2nd method \n");
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i] + ". \n");
+    console.log("Hi, " + names[i] + " I hope you will be fine! \n");
+}
