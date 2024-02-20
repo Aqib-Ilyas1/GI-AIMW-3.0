cddb0fbf5359890bfae77ac380f03d39f165a27a
Aqib Ilyas <aqibilyas289@gmail.com>
2024-02-20 10:56:34 +0500

    Exercise no 21
    They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.

---
+++ Module (1) - TypeScript/Ex#21/main.js
@@ -0,0 +1,29 @@
+var citiesofPakistan = {
+    Karachi: {
+        direction: "Southern Pakistan",
+        citycode: "021",
+        population: "14.91 million"
+    },
+    Lahore: {
+        direction: "North-eastern region of Punjab",
+        citycode: "042",
+        population: "11.13 million"
+    },
+    Multan: {
+        direction: "Southern part of Punjab",
+        citycode: "061",
+        population: "1.872 million"
+    },
+    Rawalpindi: {
+        direction: "Northern Pakistan",
+        citycode: "051",
+        population: "2.098 million"
+    },
+    Sukkur: {
+        direction: "Southeastern Pakistan",
+        citycode: "+92",
+        population: "499,900"
+    }
+};
+console.log("Cities of Pakistan \n");
+console.log(citiesofPakistan);
